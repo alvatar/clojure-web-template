f84674a8849edba8ce58e3c533f2bb6eef26602c
Alvaro Castro-Castilla <alvcastro@yahoo.es>
2017-09-02 16:48:00 +0200

    Init script

---
+++ init-project.sh
@@ -0,0 +1,23 @@
+#!/bin/env sh
+
+if [ "$#" -ne 1 ]; then 
+  echo "Illegal number of parameters"
+  exit 1
+fi
+
+NEW_NAME=$1
+
+printf "Cleaning up...\n"
+
+lein clean
+
+printf "Renaming to: %s...\n" $1 
+
+#find . -type f | xargs sed -i "s/myproject/$NEW_NAME/g"
+for i in $(find . -type d -iname "*myproject*")
+do
+  echo "$i"
+  mv "$i" "${i//myproject//$NEW_NAME}"
+done
+
+printf "Done.\n"
--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject myproject "0.1.0-SNAPSHOT"
-  :description "Myproject"
+  :description "myproject"
   :url "http://myproject.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
