ec10aa731571c1fdd1907a584371d18a71fd1d89
Alvaro Castro-Castilla <alvcastro@yahoo.es>
2017-09-03 20:44:55 +0200

    Added Gridlex CSS dp

--- src/clj/myproject/html.clj
+++ src/clj/myproject/html.clj
@@ -11,7 +11,9 @@
      ;; Normalize
      [:link {:rel "stylesheet" :href "https://necolas.github.io/normalize.css/7.0.0/normalize.css"}]
      ;; Awesome Font
-     [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"}]]
+     [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"}]
+     ;; Gridlex
+     [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.4.0/gridlex.min.css"}]]
     [:body
      [:div#app]
      [:script {:src "js/compiled/myproject.js" :type "text/javascript"}]]]))
